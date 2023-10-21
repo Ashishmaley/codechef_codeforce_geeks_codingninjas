603abb1ae0a4b6be2c79bfc8e06e4ef1a5c245dc
I'm ÀŚHÌŚH  MÀŁEY <90534593+Ashishmaley@users.noreply.github.com>
2023-10-21 15:56:04 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sum of all divisors from 1 to n/sum-of-all-divisors-from-1-to-n.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    long long sumOfDivisors(int n)
+    {
+	    long long divSum = 0;
+        for (int i = 1; i <= n; i++) {
+            divSum += i * (n / i);
+         }
+       return divSum;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        long long ans  = ob.sumOfDivisors(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
